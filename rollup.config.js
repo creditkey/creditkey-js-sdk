fed089a8c1e85311bc12cec8e686f6f4364a0985
Eben Goodman <eben.goodman@gmail.com>
2022-06-17 16:13:24 -0400

    setup modal and post message callbacks

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
+import babel from 'rollup-plugin-babel';
+
 export default [
   {
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+      }),
+    ],
     input: 'index.js',
-    output: {
+    output: [{
       file: 'build/index.js',
       format: 'cjs'
-    }
+    }, {
+      file: 'build/bundle.js',
+      format: 'umd',
+      name: 'ck',
+    }]
   }
 ]
