2ed50e0ba561f1d859b4e51e2026f0eee39389d4
cjdeg <cj@benfredaconsulting.com>
2022-11-16 10:20:00 -0500

    fixed modal height issue

--- modules/iframe.js
+++ modules/iframe.js
@@ -1,9 +1,7 @@
 import { actions, urls } from '../settings';
 
 export const iframe = url => {
-  return `<div id="creditkey-wrapper">
-            <iframe allowtransparency="true" scrolling="no" id="creditkey-iframe" frameBorder="0" src="${url}" width="100%"></iframe>
-          </div>`;
+  return ` src="${url}" `;
 }
 
 export const iframeCallback = (data, state) => {
