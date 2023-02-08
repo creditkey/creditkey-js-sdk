6db87ddf198491659431e0055583e565e7b104f1
Eben Goodman <eben.goodman@gmail.com>
2023-02-08 10:55:28 -0500

    fix apply behavior

--- modules/iframe.js
+++ modules/iframe.js
@@ -1,13 +1,11 @@
 import { actions, urls } from '../settings';
 
 export const iframe = url => {
-  console.log(url);
   return `<div id="creditkey-wrapper">
             <iframe allowtransparency="true" scrolling="no" id="creditkey-iframe" frameBorder="0" src="${url}" width="100%"></iframe>
           </div>`;
 }
 
 export const iframeCallback = (data, state) => {
-  console.log(state);
   if (data.action === 'pdp') actions[state.action](state);
 }
